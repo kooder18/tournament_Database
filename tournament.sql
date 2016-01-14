6af4951b25aca2417e7036073ed8e97734e76669
root <derekkooyman@gmail.com>
2016-01-14 02:02:13 -0700

    Added views to support players with zero wins and zero matches

--- tournament.sql
+++ tournament.sql
@@ -31,16 +31,55 @@ CREATE TABLE match ( matchID SERIAL primary key,
 
 
 
---This is a view called v_win takes the number of wins a player has
+--This is a view called v_win that counts the number of total wins each player has
 CREATE VIEW v_win as SELECT playerList.name, count(playerList.name) as
                        win_total FROM playerList, match WHERE playerList.id=match.winner
-                       GROUP BY playerList.name ORDER BY win_total desc;
+                       GROUP BY playerList.name;
+                       -- ORDER BY win_total desc;
 
 --This is a view called v_match it adds the number of wins and losses to get the total matches played.
 CREATE VIEW v_match as SELECT playerList.name, count(playerList.name) as
                         v_match FROM playerList, match WHERE playerList.id=match.winner OR
-                        playerList.id=match.loser GROUP BY playerList.name ORDER BY v_match desc;
+                        playerList.id=match.loser GROUP BY playerList.name;
+                        --ORDER BY v_match desc;
 
+/*This view makes a left join of the player table with the  v_win view,
+the reason it does this is to include all of the players including those
+who did not win any matches.
+*/
+CREATE VIEW v_allwin as SELECT playerList.*, v_win.win_total FROM playerList LEFT
+                              OUTER JOIN v_win ON playerList.name = v_win.name;
+
+
+/*
+This view performs a left join on the v_match table with the playerList table
+the reason this is done is to include any players who did not play any matches.
+*/
+CREATE VIEW v_allmatch as SELECT playerList.*, v_match.v_match FROM playerList
+                                LEFT OUTER JOIN v_match ON
+                                playerList.name = v_match.name;
+
+/*
+This view takes the allwin view, and runs the coalesce function on it,
+the reason for this is to replace all null values of players who have not
+won any matches with zero So we will not need to check for this in the python
+code.
+*/
+CREATE VIEW v_winf as SELECT *, COALESCE(win_total, 0) AS wins FROM
+                              v_allwin;
+
+
+/*
+This view takes the allmatch view, and runs coalese on it to replace all null
+values with zero.
+*/
+CREATE VIEW v_matchf as SELECT *, COALESCE(v_match,0) AS match
+                               FROM v_allmatch;
+
+--This is the final view with all the pieces in the table
+CREATE VIEW v_final as SELECT v_winf.id, v_winf.name, v_winf.wins, v_matchf.match
+                              FROM v_winf LEFT OUTER JOIN v_matchf ON
+                              v_winf.name = v_matchf.name ORDER BY wins desc;
 
 /* Simple test data to test out SQL queries
 This first block adds 16 different players to the
@@ -69,7 +108,7 @@ INSERT INTO playerList (name) VALUES('Grumpyman');
 More test data, this data updates the matches table
 */
 
-INSERT INTO match (winner, loser) VALUES (1, 2);
+INSERT INTO match (winner, loser) VALUES (1, 3);
 INSERT INTO match (winner, loser) VALUES (3, 4);
 INSERT INTO match (winner, loser) VALUES (5, 6);
 INSERT INTO match (winner, loser) VALUES (7, 8);
@@ -77,7 +116,7 @@ INSERT INTO match (winner, loser) VALUES (9, 10);
 INSERT INTO match (winner, loser) VALUES (11, 12);
 INSERT INTO match (winner, loser) VALUES (13, 14);
 INSERT INTO match (winner, loser) VALUES (15, 16);
-INSERT INTO match (winner, loser) VALUES (2, 4);
+INSERT INTO match (winner, loser) VALUES (5, 4);
 INSERT INTO match (winner, loser) VALUES (6, 8);
 INSERT INTO match (winner, loser) VALUES (10, 12);
 INSERT INTO match (winner, loser) VALUES (14, 16);
